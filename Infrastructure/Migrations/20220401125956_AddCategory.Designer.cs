4a1c5239e7f346d9c1965821998448a838365d9f
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2022-04-01 13:15:57 +0000

    feat(Persistence): Add category migrations

--- Infrastructure/Migrations/20220401125956_AddCategory.Designer.cs
+++ Infrastructure/Migrations/20220401125956_AddCategory.Designer.cs
@@ -2,15 +2,17 @@
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220401125956_AddCategory")]
+    partial class AddCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,3 @@
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Core.Contracts;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
