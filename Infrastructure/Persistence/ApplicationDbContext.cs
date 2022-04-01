b204e587cbb9ca3ac0e2a5575d1959599ccb675f
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2022-04-01 13:14:41 +0000

    Clean ApplicationDbContext for filtering the queries

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -26,12 +26,6 @@ namespace Infrastructure.Persistence
             TenantId = _tenantService.GetTenant()?.TID;
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Product>().HasQueryFilter(a => a.TenantId == TenantId);
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var tenantConnectionString = _tenantService?.GetConnectionString();
@@ -43,23 +37,6 @@ namespace Infrastructure.Persistence
                     optionsBuilder.UseNpgsql(_tenantService.GetConnectionString());
                 }
             }
-        }
-
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-        {
-            foreach (var entry in ChangeTracker.Entries<IMustHaveTenant>().ToList())
-            {
-                switch (entry.State)
-                {
-                    case EntityState.Added:
-                    case EntityState.Modified:
-                        entry.Entity.TenantId = TenantId;
-                        break;
-                }
-            }
-
-            var result = await base.SaveChangesAsync(cancellationToken);
-            return result;
         }
     }
 }
