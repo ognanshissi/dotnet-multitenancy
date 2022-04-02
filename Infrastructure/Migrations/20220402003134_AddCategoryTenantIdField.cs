a36a681f5a561587a94e17a0d427ac49d46c1788
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2022-04-02 00:39:15 +0000

    feat(Entity): Add tenantId field in category for shared database use cas

---
+++ Infrastructure/Migrations/20220402003134_AddCategoryTenantIdField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddCategoryTenantIdField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                table: "Categories",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Categories");
+        }
+    }
+}
